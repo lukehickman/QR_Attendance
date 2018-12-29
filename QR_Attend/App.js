52b54f53232de75177d205b89103e649a8e4d5b0
ElyYonce <elyyonce11@gmail.com>
2018-12-28 19:41:49 -0500

    added a text box to the login screen

--- QR_Attend/App.js
+++ QR_Attend/App.js
@@ -7,23 +7,22 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
+import {Platform, StyleSheet, Text, TextInput, View} from 'react-native';
 export default class App extends Component<Props> {
-  render() {
+    constructor(props) {
+        super(props);
+        this.state = { text: 'Useless Placeholder' };
+    }
+
+    render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Welcome to the QR Attendance application</Text>
+      <TextInput
+      style={{width: 200, height: 40, borderColor: 'gray', borderWidth: 1}}
+      onChangeText={(text) => this.setState({text})}
+      value={this.state.text}
+      />
       </View>
     );
   }
